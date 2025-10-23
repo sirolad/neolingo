e61c201abcc046919d0bbf22c6cd90d8819d0b8d
Surajudeen Akande <sirolad@gmail.com>
2025-10-23 13:48:43 +0200

    (feat) - add sentry

--- src/instrumentation-client.ts
+++ src/instrumentation-client.ts
@@ -1,15 +1,14 @@
-// This file configures the initialization of Sentry on the server.
-// The config you add here will be used whenever the server handles a request.
+// This file configures the initialization of Sentry on the client.
+// The added config here will be used whenever a users loads a page in their browser.
 // https://docs.sentry.io/platforms/javascript/guides/nextjs/
 
 import * as Sentry from '@sentry/nextjs';
 
 Sentry.init({
-  dsn: 'https://88c5e722ed3ba31494e28e6dfe352bd2@o424054.ingest.us.sentry.io/4510238458904576',
+  dsn: process.env.NEXT_PUBLIC_SENTRY_DSN,
 
   // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
   tracesSampleRate: 1,
-
   // Enable logs to be sent to Sentry
   enableLogs: true,
 
@@ -17,3 +16,5 @@ Sentry.init({
   // https://docs.sentry.io/platforms/javascript/guides/nextjs/configuration/options/#sendDefaultPii
   sendDefaultPii: true,
 });
+
+export const onRouterTransitionStart = Sentry.captureRouterTransitionStart;
